1b41172520f08e75a4a3ef3bc9a2a6f447d2ccca
patr4519 <jero96@mail.ru>
2023-02-22 16:49:14 +0300

    +

--- src/components/Plug.jsx
+++ src/components/Plug.jsx
@@ -7,12 +7,15 @@ const Plug = (props) => (
     width={350}
     height={125}
     viewBox="0 0 350 125"
-    backgroundColor="#3a3d40"
-    foregroundColor="#ecebeb"
+    backgroundColor="#284c57"
+    foregroundColor="#417168"
     {...props}
   >
-    <rect x="0" y="0" rx="20" ry="20" width="350" height="125" /> 
-    <rect x="0" y="210" rx="20" ry="20" width="350" height="100" />
+    <rect x="0" y="0" rx="5" ry="5" width="350" height="22" /> 
+    <rect x="0" y="30" rx="5" ry="5" width="320" height="22" /> 
+    <rect x="0" y="59" rx="5" ry="5" width="250" height="22" /> 
+    <rect x="0" y="90" rx="5" ry="5" width="150" height="22" /> 
+    <rect x="290" y="90" rx="5" ry="5" width="60" height="25" />
   </ContentLoader>
 )
 
